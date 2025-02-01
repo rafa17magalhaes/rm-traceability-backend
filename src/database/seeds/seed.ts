f1eec4d3b1815cec576b5924bfaab5b14716ce3b
Rafael <rafael17magalhaes@gmail.com>
2025-02-01 05:45:55 -0300

    removed: test

--- src/database/seeds/seed.ts
+++ src/database/seeds/seed.ts
@@ -1,4 +1,3 @@
-// src/database/seeds/seed.ts
 import { createConnection, Connection, Repository } from 'typeorm';
 import { Company } from 'src/companies/entities/company.entity';
 import { User } from 'src/users/entities/user.entity';
@@ -10,20 +9,17 @@ export async function seed(): Promise<void> {
   console.log('Conexão estabelecida.');
 
   try {
-    // Obtém os repositórios para Company e User
     const companyRepo: Repository<Company> = connection.getRepository(Company);
     const userRepo: Repository<User> = connection.getRepository(User);
 
-    // Verifica se já existe a empresa "Smart Invisible"
     let existingCompany = await companyRepo.findOne({ where: { name: 'Smart Invisible' } });
     if (!existingCompany) {
       // Cria uma nova empresa
       existingCompany = companyRepo.create({
-        code: 'SMART123', // Adicione um código apropriado
-        document: '000000000', // Exemplo: CNPJ, CPF, etc.
+        code: 'SMART123',
+        document: '000000000',
         name: 'Smart Invisible',
         trade: 'Smart Invisible Trade',
-        // Adicione outros campos conforme necessário
       });
       await companyRepo.save(existingCompany);
       console.log('Empresa "Smart Invisible" criada.');
@@ -31,15 +27,13 @@ export async function seed(): Promise<void> {
       console.log('Empresa "Smart Invisible" já existe.');
     }
 
-    // Verifica se já existe o usuário "Rafael"
     const existingUser = await userRepo.findOne({ where: { email: 'admin@padrao.com' } });
     if (!existingUser) {
-      // Cria um usuário admin associado à empresa
       const newUser = userRepo.create({
         name: 'Rafael',
         email: 'admin@padrao.com',
         phone: '11999999999',
-        password: '123456', // Senha em texto plano; será hashada pelo hook
+        password: '123456',
         company: existingCompany,
       });
       await userRepo.save(newUser);
@@ -50,7 +44,6 @@ export async function seed(): Promise<void> {
   } catch (error) {
     console.error('Erro durante o seed:', error);
   } finally {
-    // Fecha a conexão com o banco de dados
     await connection.close();
     console.log('Conexão fechada.');
   }
