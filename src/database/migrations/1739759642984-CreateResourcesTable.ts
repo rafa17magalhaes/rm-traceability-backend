012812742af25c844338ac3616b6f2c3dd03f0bd
Rafael <rafael17magalhaes@gmail.com>
2025-02-17 00:06:26 -0300

    Feat(migrations): create new resources table.
    - Add migration to create the "resources" table.

---
+++ src/database/migrations/1739759642984-CreateResourcesTable.ts
@@ -0,0 +1,52 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateResourcesTable1739759642984 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'resources',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            isNullable: false,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'description',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'active',
+            type: 'boolean',
+            default: true,
+          },
+          {
+            name: 'company_id',
+            type: 'varchar',
+            isNullable: false,
+          },
+        ],
+        uniques: [
+          {
+            columnNames: ['name', 'company_id'],
+            name: 'UQ_resources_name_company_id',
+          },
+        ],
+      }),
+      true,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('resources');
+  }
+}
